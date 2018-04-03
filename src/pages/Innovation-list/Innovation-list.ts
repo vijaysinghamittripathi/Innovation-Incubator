2a084082872c722b3f3a27b7d4ce67defab9fba1
Amit Tripathi <amit.tripathi@klm.com>
2018-04-03 17:22:45 +0200

    Changed title as per category and innovation

--- src/pages/Innovation-list/Innovation-list.ts
+++ src/pages/Innovation-list/Innovation-list.ts
@@ -3,7 +3,7 @@ import {Config, NavController, ModalController, NavParams} from 'ionic-angular';
 
 
 import { WalkthroughModalPage } from '../popup/popup-modal';
-import {  Innovation } from '../../models/innovation';
+import { Content, Innovation, Team } from '../../models/innovation';
 import { InnovationDetailsPage } from '../innovation-details/innovation-details';
 import { InnovationsService } from '../../providers/innovations-service';
 
@@ -13,15 +13,16 @@ import { InnovationsService } from '../../providers/innovations-service';
 })
 export class InnovationListPage {
 
-    public innovationdata: Array<any[]>;
+    public innovationdata: Innovation[];
+    public categorydata;
 
     constructor(public navCtrl: NavController, public config: Config,
         public modalCtrl: ModalController, public navParams: NavParams, public innovationsService: InnovationsService) {
     }
 
     ionViewWillLoad() {
-        let categorydata = this.navParams.get('item');
-        let selectedCategory = categorydata.category_id;
+        this.categorydata = this.navParams.get('item');
+        let selectedCategory = this.categorydata.category_id;
         this.getInnovationsPerCategory(selectedCategory);
     }
 
@@ -32,7 +33,7 @@ export class InnovationListPage {
             this.innovationdata = [];
             _items.forEach(item => {
                 if( item.cat_innovation_id == categoryID) {
-                    this.innovationdata.push(item);
+                    this.innovationdata.push(item as Innovation);
                 } 
             })
         }); 
