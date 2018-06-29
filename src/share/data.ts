31eb89230cf6ad8ad911eea4bdc5b4c8de4a9e0d
Amit Tripathi <amit.tripathi@klm.com>
2018-06-29 11:51:43 +0200

    Added data string-modified

--- src/share/data.ts
+++ src/share/data.ts
@@ -1,6 +1,14 @@
 export const Data: any = {
+<<<<<<< HEAD
    settings: {
         minFontSize: 18,
         maxFontSize: 96,
+=======
+    data: 'IOT BLOCKCHAIN VR ROBOTICS WEB AI',
+    settings: {
+        minFontSize: 20,
+        maxFontSize: 45,
+        datastring:'WORLD CLOUD'
+>>>>>>> d5c9014... Added data string
     }
 };
